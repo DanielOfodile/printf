ac6da91df59b31cd098f552d19abffb82c227eab
Daniel <ofodiledaniel20@gmail.com>
2022-05-19 14:37:15 -0700

    creates _printf.c

---
+++ _printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf - personalised printf function.
+ * @format: string to be printed.
+ *
+ * Return: number of chars that print
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int displayed = 0;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(args, format);
+
+	displayed = _sellect_format(format, args);
+	va_end(args);
+	return (displayed);
+}
